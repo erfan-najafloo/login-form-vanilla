ba7086be3e9f22b10ea7c9233319185c702a951b
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-21 18:02:53 +0430

    Add basic input validation and display messages

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const emailInput = document.querySelector('.email-input');
 const passwordInput = document.querySelector('.password-input');
 const loginButton = document.querySelector('.login-button');
+const outputMessage = document.querySelector('.output-message');
 loginButton.addEventListener(
-    'click',()=>{
+    'click', () => {
+        if (emailInput.value === '' && passwordInput.value === '') {
+            outputMessage.textContent = 'Email and password are required'
+        }
+        else if (emailInput.value === '') {
+            outputMessage.textContent = 'Please enter your Email'
+        } else if (passwordInput.value === '') {
+            outputMessage.textContent = 'Please enter your password'
+        } else {
+            outputMessage.textContent = "Login successful"
+        }
 
-
-    })
+    });
