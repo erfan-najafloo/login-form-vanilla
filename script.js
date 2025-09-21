1d68611d7b54c3a557b45c205c21e2b7c18e60d1
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-21 11:24:36 +0430

    Select input fields and setup login button listener

---
+++ script.js
@@ -0,0 +1,8 @@
+const emailInput = document.querySelector('.email-input');
+const passwordInput = document.querySelector('.password-input');
+const loginButton = document.querySelector('.login-button');
+loginButton.addEventListener(
+    'click',()=>{
+
+
+    })
